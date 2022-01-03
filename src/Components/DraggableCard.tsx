99d764251888c3ace1192c3d2def486d1143d5a1
Yongveloper <aaa9015@naver.com>
2022-01-03 21:57:21 +0900

    Performance(React.memo)

---
+++ src/Components/DraggableCard.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Draggable } from 'react-beautiful-dnd';
+import styled from 'styled-components';
+
+const Card = styled.div`
+  border-radius: 5px;
+  padding: 10px 10px;
+  margin-bottom: 5px;
+  background-color: ${(props) => props.theme.cardColor};
+`;
+
+interface IDraggableProps {
+  toDo: string;
+  index: number;
+}
+
+function DraggableCard({ toDo, index }: IDraggableProps) {
+  console.log(toDo, 'has been renderd');
+  return (
+    <Draggable draggableId={toDo} index={index} key={toDo}>
+      {(magic) => (
+        <Card
+          ref={magic.innerRef}
+          {...magic.dragHandleProps}
+          {...magic.draggableProps}
+        >
+          {toDo}
+        </Card>
+      )}
+    </Draggable>
+  );
+}
+
+export default React.memo(DraggableCard);
